98204d45d4cbe9f1b1f75f5152176778b118ef10
Daniil <dani2003il@mail.ru>
2024-12-01 15:38:20 +0700

    ci: project initialization

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const input = ref('')
+</script>
+<template>
+    <el-input v-model="input" style="width: 240px" placeholder="Please input" clearable />
+</template>
+<style scoped lang="scss">
+.text {
+    &__main {
+        color: red;
+        font-family: 'Raleway', sans-serif;
+    }
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+<template>
+    <header></header>
+</template>
+<style scoped lang="scss"></style>
